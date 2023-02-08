d68fd614ce492e9572661338fb5ea24e911de339
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-08 19:08:38 +0100

    Assert that we can fetch rates for a given date.

--- src/BNRExchangeHistory.test.ts
+++ src/BNRExchangeHistory.test.ts
@@ -8,6 +8,15 @@ describe('BNRExchangeHistory', () => {
         expect(rates.USD.rate).toBeGreaterThan(0);
         expect(rates.USD.multiplier).toBeGreaterThan(0);
         expect(rates.USD.name).toBe('USD');
+    });
+    
+    it('can fetch exchange rates for a specific date', async () => {
+        const Feb5_2023 = 1675620351166;
+        const expectedRate = 4.4632;
+        
+        const rates = await BNRExchangeHistory.getRates({ date: new Date(Feb5_2023) });
+        
+        expect(rates.USD.rate).toBe(expectedRate);
     })
     
 })
