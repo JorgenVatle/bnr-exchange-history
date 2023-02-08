1ce1bea3bad070d320d0fa44387bd451dd19ad55
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-08 19:20:24 +0100

    Correct test for non-banking day exchange rates.

--- src/BNRExchangeHistory.test.ts
+++ src/BNRExchangeHistory.test.ts
@@ -10,11 +10,13 @@ describe('BNRExchangeHistory', () => {
         expect(rates.USD.name).toBe('USD');
     });
     
-    it('can fetch exchange rates for a specific date', async () => {
-        const Feb5_2023 = 1675620351166;
-        const expectedRate = 4.4632;
+    it('can fetch exchange rates for non-banking days', async () => {
+        const Sunday_Feb_5_2023 = 1675620351166;
+        const expectedRate = 4.4823; // Should be the rate from Feb 3, 2023.
         
-        const rates = await BNRExchangeHistory.getRates({ date: new Date(Feb5_2023) });
+        const rates = await BNRExchangeHistory.getRates({
+            date: new Date(Sunday_Feb_5_2023)
+        });
         
         expect(rates.USD.rate).toBe(expectedRate);
     })
