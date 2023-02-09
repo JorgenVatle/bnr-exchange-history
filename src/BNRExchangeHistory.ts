362aaf06b120115d93bc9610c0b8812362cd07b6
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-09 01:35:08 +0100

    Simplify year traversal in BNRExchangeHistory.

--- src/BNRExchangeHistory.ts
+++ src/BNRExchangeHistory.ts
@@ -10,30 +10,11 @@ class BNRExchangeHistory {
             invoice = false,
         },
     ) {
-        const moment = Moment(date);
-        const year = date.getFullYear();
         const maxDaysToTraverse = 25;
-        
-        if (moment.isAfter(Date.now(), 'day')) {
-            throw new BNRError(`The provided date "${moment.calendar()}" appears to be in the future!`);
-        }
-        
-        let exchangeYear = await ExchangeYear.for({ year }).catch((error) => {
-            if (!(error instanceof InvalidBNRResponse)) {
-                throw error;
-            }
-            
-            if (!moment.isSame(Date.now(), 'year')) {
-                throw error;
-            }
-            
-            const lastYear = year - 1;
-            
-            return ExchangeYear.for({ year: lastYear });
-        });
     
         for (let daysToSubtract = 0; daysToSubtract < maxDaysToTraverse; daysToSubtract++) {
             const moment = Moment(date).subtract(daysToSubtract, 'days');
+            const exchangeYear = await this.getExchangeYear(moment);
             const exchangeDay = exchangeYear.getDay(moment.toDate());
         
             if (invoice && moment.isSame(date, 'day')) {
@@ -43,13 +24,33 @@ class BNRExchangeHistory {
             if (exchangeDay) {
                 return exchangeDay.getRates();
             }
-        
-            if (!exchangeYear.sameYear(moment.toDate())) {
-                exchangeYear = await ExchangeYear.for({ year: moment.year() });
-            }
         }
     
         throw new BNRError('Date is out of range. Needs to be between today and Jan 3rd, 2005!');
+    }
+    
+    protected validateDate(date: Moment.Moment) {
+        if (date.isAfter(Date.now(), 'day')) {
+            throw new BNRError(`The provided date "${date.calendar()}" appears to be in the future!`);
+        }
+    }
+    
+    protected getExchangeYear(date: Moment.Moment) {
+        this.validateDate(date);
+        
+        return ExchangeYear.for({ year: date.year() }).catch((error) => {
+            if (!(error instanceof InvalidBNRResponse)) {
+                throw error;
+            }
+        
+            if (!date.isSame(Date.now(), 'year')) {
+                throw error;
+            }
+        
+            const lastYear = date.year() - 1;
+        
+            return ExchangeYear.for({ year: lastYear });
+        });
     }
     
 }
