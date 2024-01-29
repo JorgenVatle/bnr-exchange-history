e4d8021ff80c1ad6110c1e720fd9198f7769acee
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-29 19:16:15 +0100

    Set build target to 'node12'

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
+    target: 'node12',
     outDir: 'dist',
     dts: true,
     sourcemap: true,
